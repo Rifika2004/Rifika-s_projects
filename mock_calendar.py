4e3e1ec022c3b9f804febaacc2814a5ff28d8b3f
Rifika Garigipati <rifikagarigipati@gmail.com>
2025-06-28 21:10:44 +0530

    ➕ Added mock_calendar module

--- mock_calendar.py
+++ mock_calendar.py
@@ -1,17 +1,9 @@
-# mock_calendar.py
-import datetime
+from datetime import datetime
 
-class MockCalendar:
-    def __init__(self):
-        self.bookings = []
+calendar = {}
 
-    def is_free(self, start: datetime.datetime, end: datetime.datetime) -> bool:
-        return all(end <= b["start"] or start >= b["end"] for b in self.bookings)
+def is_available(dt: datetime) -> bool:
+    return dt not in calendar
 
-    def book(self, start: datetime.datetime, end: datetime.datetime, summary: str):
-        if not self.is_free(start, end):
-            return False
-        self.bookings.append({"start": start, "end": end, "summary": summary})
-        return True
-
-calendar = MockCalendar()
+def book_slot(dt: datetime):
+    calendar[dt] = True
